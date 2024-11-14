540795d0f84a4134b4f7bcee01e054e15b3073bc
Vincent Chantreau <vincent.chantreau@outlook.fr>
2024-11-14 23:26:31 +0100

    fix

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "VincentChantreau/prime-cache/cmd"
+import "github.com/VincentChantreau/prime-cache/cmd"
 
 func main() {
 	cmd.Execute()
